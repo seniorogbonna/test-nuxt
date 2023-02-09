e58e8675f4b1bb6c207fbee33222a48d9f7e7070
DESKTOP-KM80R2C\kkk <seniorogbonna@gmail.com>
2023-02-09 17:17:43 +0000

    align items center

--- components/CourseItem.vue
+++ components/CourseItem.vue
@@ -2,15 +2,15 @@
   <div class="d-flex w-60 accordion-item-section pointer">
     <svg fill="#5624d0" height="14px" width="14px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
       xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330 330" xml:space="preserve"
-      style="margin-left: 4px; padding: 9px;">
+      style="margin-left: 4px; padding: 10px;">
       <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
 	c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
 	s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z" />
     </svg>
     <div class="flex-full item-title">{{ courseItem.title }}</div>
     <div class="d-flex">
-      <div class="text-sections">{{ courseItem.lectures }} lectures</div>
-      <div class="pl-1">{{ courseItem.time }}min</div>
+      <div class="text-sections d-flex align-items-center">{{ courseItem.lectures }} lectures</div>
+      <div class="pl-1 d-flex align-items-center">{{ courseItem.time }}min</div>
     </div>
   </div>
   <div v-if="courseItem.expanded">
